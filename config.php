29bae489d35f854f2beab8970a63332a6174b8cf
Marc Mascort Bou <marc@marcmascort.com>
2015-03-28 11:12:18 +0100

    Config

---
+++ config.php
@@ -0,0 +1,45 @@
+<?php
+
+/*******************************************************************************
+ * DB
+ *******************************************************************************/
+
+define('DB_HOST',       'localhost');
+define('DB_NAME',       'hackajobs');
+define('DB_USER',       'root');
+define('DB_PASSWORD',   '');
+
+/*******************************************************************************
+ * FACEBOOK
+ *******************************************************************************/
+
+define('FB_APP_ID', '898845680138958');
+define('FB_APP_SECRET', 'fcd7c90480cfcbe5cb5671fc5c9ff281');
+
+// ----------------------------------------------------------------------------------------
+//	HybridAuth Config file: http://hybridauth.sourceforge.net/userguide/Configuration.html
+// ----------------------------------------------------------------------------------------
+
+$oauthConf = array(
+    'base_url' => 'http://apisocial.wallyjobs.com/vendor/hybridauth/',
+
+    'providers' => array (
+        'Facebook' => array (
+            'enabled' => true,
+            'keys'    => array(
+                'id' => FB_APP_ID,
+                'secret' => FB_APP_SECRET
+            ),
+            'trustForwarded' => false
+        ),
+    ),
+
+    // If you want to enable logging, set 'debug_mode' to true.
+    // You can also set it to
+    // - 'error' To log only error messages. Useful in production
+    // - 'info' To log info and error messages (ignore debug messages)
+    'debug_mode' => false,
+
+    // Path to file writable by the web server. Required if 'debug_mode' is not false
+    'debug_file' => '',
+);
--- index.php
+++ index.php
@@ -1,30 +1,25 @@
-<html>
-	<head></head>
-	<body>
-  <?php
-		// Crida a les configs de social
-    $oauthConf = dirname(__FILE__) . '/vendor/hybridauth/config.php';
+<?php
+// Configuration
+require_once dirname(__FILE__) . 'config.php';
 
-		require_once(dirname(__FILE__) . '/vendor/autoload.php');
-		
+// Autoload
+require_once dirname(__FILE__) . '/vendor/autoload.php';
 
-   try{
 
-       $hybridauth = new Hybrid_Auth( $oauthConf );
- 
-       $facebook = $hybridauth->authenticate( "Facebook" );
- 
-       $user_profile = $facebook->getUserProfile();
- 
-       echo "Hi there! " . $user_profile->displayName;
- 
-       $facebook->setUserStatus( "Hello world!" );
- 
-       $user_contacts = $facebook->getUserContacts(); 
-   }
-   catch( Exception $e ){
-       echo "Ooophs, we got an error: " . $e->getMessage();
-   }
-   ?>
-	</body>
-</html>
+/*try{
+
+   $hybridauth = new Hybrid_Auth( $oauthConf );
+
+   $facebook = $hybridauth->authenticate( "Facebook" );
+
+   $user_profile = $facebook->getUserProfile();
+
+   echo "Hi there! " . $user_profile->displayName;
+
+   $facebook->setUserStatus( "Hello world!" );
+
+   $user_contacts = $facebook->getUserContacts(); 
+}
+catch( Exception $e ){
+   echo "Ooophs, we got an error: " . $e->getMessage();
+}*/
