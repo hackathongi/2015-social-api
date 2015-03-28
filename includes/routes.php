d53655ca7bbe7ac11006b07515a0ddfc58b285c1
Marc Mascort Bou <marc@marcmascort.com>
2015-03-28 18:37:01 +0100

    Info social

--- includes/routes.php
+++ includes/routes.php
@@ -117,6 +117,43 @@
             $app->response->body(json_encode($response));
         }
     });
+	
+	/**
+	 * Tornar la info de l'usuari
+	 */
+	$app->get('/info-social/:provider/:userId', function ($provider, $user_id)  use ($app){
+        global $oauthConf;
+        if (is_allowed_provider($provider))
+        {
+            $db = new DB();
+            $user = $db->getUserBySocialId($user_id);
+            if ($user)
+            {
+                $app->response->headers->set('Content-Type', 'application/json');
+                $app->response->setStatus(200);
+                $app->response->body(json_encode($user));
+            }
+            else
+            {
+                $app->response->headers->set('Content-Type', 'application/json');
+                $app->response->setStatus(500);
+                $response = array(
+                  'message' => 'Unknown user',
+                  'code' 		=> 500,
+                );
+                $app->response->body(json_encode($response));
+            }
+        }
+        else {
+            $app->response->headers->set('Content-Type', 'application/json');
+            $app->response->setStatus(500);
+            $response = array(
+              'message' => 'Unknown provider',
+              'code' 		=> 500,
+            );
+            $app->response->body(json_encode($response));
+        }
+    });
 
 	/**
 	 * Tornar els amics de l'usuari
