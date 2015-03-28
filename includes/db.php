f64f38db93ef22c4e29484a3de88ae5b84c53a2b
Marc Mascort Bou <marc@marcmascort.com>
2015-03-28 13:53:42 +0100

    DB user + contact

--- includes/db.php
+++ includes/db.php
@@ -26,6 +26,15 @@ class DB
     }
     
     /**
+     * Get last query
+     * @return string
+     */
+    function getLastQuery()
+    {
+        return $this->database->last_query();
+    }
+    
+    /**
      * Get user data by ID
      * @param int $user_id
      * @return array|bool
@@ -36,6 +45,20 @@ class DB
             static::TABLE_USER,
             ['id', 'name', 'email', 'token', 'role', 'facebook_id'],
             ['id'=> intval($user_id)]
+        );
+    }
+    
+    /**
+     * Get users data byID
+     * @param array $user_ids
+     * @return array
+     */
+    public function getUsers($user_ids)
+    {
+        return $this->database->select(
+            static::TABLE_USER,
+            ['id', 'name', 'email', 'token', 'role', 'facebook_id'],
+            ['id'=> is_array($user_ids) ? $user_ids : array($user_ids)]
         );
     }
     
@@ -83,7 +106,7 @@ class DB
     
     /**
      * Update user data by Social ID
-     * @param int $user_id
+     * @param int $user_social_id
      * @param array $data
      * @return int
      */
@@ -93,6 +116,97 @@ class DB
             static::TABLE_USER,
             $data,
             ['facebook_id'=> intval($user_social_id)]
+        );
+    }
+    
+    /**
+     * Get Contact by ID
+     * @param int $contact_id
+     * @return array|bool
+     */
+    public function getContact($contact_id)
+    {
+        return $this->database->get(
+            static::TABLE_CONTACT,
+            ['id', 'name', 'picture_url', 'facebook_id', 'user_id'],
+            ['id'=> intval($contact_id)]
+        );
+    }
+    
+    /**
+     * Get all contacts of user by User ID
+     * @param int $user_id
+     * @return array|bool
+     */
+    public function getContactsByUserId($user_id)
+    {
+        return $this->database->select(
+            static::TABLE_CONTACT,
+            ['id', 'name', 'picture_url', 'facebook_id', 'user_id'],
+            ['user_id'=> intval($user_id)]
+        );
+    }
+    
+    /**
+     * Get all contacts by Social ID
+     * @param int $contact_social_id
+     * @return array
+     */
+    public function getContactsBySocialId($contact_social_id)
+    {
+        return $this->database->select(
+            static::TABLE_CONTACT,
+            ['id', 'name', 'picture_url', 'facebook_id', 'user_id'],
+            ['facebook_id'=> intval($contact_social_id)]
+        );
+    }
+    
+    /**
+     * Get all contact users by Social ID
+     * @param int $contact_social_id
+     * @return array
+     */
+    public function getContactUsersBySocialId($contact_social_id)
+    {
+        $contacts = $this->getContactsBySocialId($contact_social_id);
+        $users = [];
+        if (!empty($contacts))
+        {
+            $users_id = [];
+            foreach ($contacts AS $contact)
+            {
+                $users_id[] = $contact['user_id'];
+            }
+            $users = $this->getUsers($users_id);
+        }
+        return $users;
+    }
+    
+    /**
+     * Insert contact data
+     * @param array $data
+     * @return type
+     */
+    public function insertContact($data)
+    {
+        return $this->database->insert(
+            static::TABLE_CONTACT,
+            $data
+        );
+    }
+    
+    /**
+     * Update contact data by ID
+     * @param int $contact_id
+     * @param array $data
+     * @return int
+     */
+    public function updateContact($contact_id, $data)
+    {
+        return $this->database->update(
+            static::TABLE_USER,
+            $data,
+            ['id'=> intval($contact_id)]
         );
     }
     
